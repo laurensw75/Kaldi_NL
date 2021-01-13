fe391f2dab0fa3a4d948a2ace9129cc3a4c4b4fc
laurensw75 <laurensw75@gmail.com>
2021-01-13 13:47:22 +0100

    Update configure_basic.sh
    update because of expired certificate

--- local/configure_basic.sh
+++ local/configure_basic.sh
@@ -26,7 +26,7 @@ if [ ! -d models/NL ]; then
 	done	
 	[ ! $return_value -eq 0 ] && echo "Models not downloaded. Cancelling" && exit 1
 	mkdir -p $modelpack
-	[ ! -e $modelpack/Models_Starterpack.tar.gz ] && wget -P $modelpack http://nlspraak.ewi.utwente.nl/open-source-spraakherkenning-NL/Models_Starterpack.tar.gz
+	[ ! -e $modelpack/Models_Starterpack.tar.gz ] && wget --no-check-certificate -P $modelpack http://nlspraak.ewi.utwente.nl/open-source-spraakherkenning-NL/Models_Starterpack.tar.gz
 	tar -xvzf $modelpack/Models_Starterpack.tar.gz -C $modelpack
 	rm -rf models
 	ln -s -f $modelpack/Models models
@@ -35,7 +35,7 @@ fi
 
 if [ ! -e models/Patch1 ]; then
 	modelpack=$(readlink -f models)/..
-	[ ! -e $modelpack/Models_Patch1.tar.gz ] && wget -P $modelpack http://nlspraak.ewi.utwente.nl/open-source-spraakherkenning-NL/Models_Patch1.tar.gz
+	[ ! -e $modelpack/Models_Patch1.tar.gz ] && wget --no-check-certificate -P $modelpack http://nlspraak.ewi.utwente.nl/open-source-spraakherkenning-NL/Models_Patch1.tar.gz
 	tar -xvzf $modelpack/Models_Patch1.tar.gz -C $modelpack
 fi
 	
